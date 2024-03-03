8770d8e9b97fb2b29f867e707a813a9877128872
Álvaro <alvaro.peixoto@dcomp.ufs.br>
2024-03-03 15:56:13 -0300

    feat: configura o banco de dados na aplicação

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
+using JitCars.Data;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
 
 var app = builder.Build();
 
