3a651e0c94eb332f9527eacb6018c59a1bac8022
Álvaro <alvaro.peixoto@dcomp.ufs.br>
2024-03-03 15:55:39 -0300

    feat: cria contexto pro banco de dados

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using JitCars.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JitCars.Data
+{
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Carro> Carros { get; set; }
+        public DbSet<Modelo> Modelos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Endereco> Enderecos { get; set;}
+        public DbSet<Telefone> Telefones { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Telefone>()
+                .HasIndex(e => e.Numero)
+                .IsUnique();
+
+        }
+
+
+    }
+}
