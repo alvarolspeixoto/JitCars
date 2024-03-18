12b05e67b483045e8164f767e4a725de92cd016b
Caio <caiaoviniciuscc@gmail.com>
2024-03-18 20:02:28 -0300

    add: controller de venda

---
+++ Controllers/VendaController.cs
@@ -0,0 +1,111 @@
+﻿using JitCars.Data;
+using JitCars.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JitCars.Controllers
+{
+	public class VendaController : Controller
+	{
+		private readonly AppDbContext _db;
+
+        public VendaController(AppDbContext db)
+        {
+			_db = db;
+        }
+
+        public IActionResult Index()
+		{
+			IEnumerable<Venda> objVendaList = _db.Vendas.ToList();
+			return View(objVendaList);
+		}
+
+        //GET
+        public IActionResult Registrar()
+        {
+            
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Registrar(Venda obj)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Vendas.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        //GET
+        public IActionResult Editar(int? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+            var vendaFromDb = _db.Vendas.Find(id);
+
+            if(vendaFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(vendaFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Editar(Venda obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Vendas.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        //GET
+        public IActionResult Deletar(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var vendaFromDb = _db.Vendas.Find(id);
+
+            if (vendaFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(vendaFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletarPOST(int? id)
+        {
+           var objToDelete = _db.Vendas.Find(id);
+           if(objToDelete == null)
+           {
+                return NotFound();
+           }
+
+           _db.Vendas.Remove(objToDelete);
+           _db.SaveChanges();
+           return RedirectToAction("Index");         
+        }
+
+
+
+
+    }
+}
