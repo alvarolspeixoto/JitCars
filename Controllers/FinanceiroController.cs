8e11f421dbf9de594f8130688c4da89e62419d6f
joseadelson2002 <joseadelson2002@academico.ufs.br>
2024-03-24 11:20:44 -0300

    criação do controller e view financeiro

---
+++ Controllers/FinanceiroController.cs
@@ -0,0 +1,31 @@
+﻿using JitCars.Data;
+using JitCars.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace JitCars.Controllers
+{
+    public class FinanceiroController : Controller
+    {
+
+        private readonly AppDbContext dbase;
+
+        public FinanceiroController(AppDbContext db)
+        {
+            dbase = db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.Carros = await dbase.Carros.Include(e => e.Modelo).ToListAsync();
+            ViewBag.Vendas = dbase.Vendas.ToList();
+
+            return View();
+        }
+
+        public IActionResult ObterNome()
+        {
+           
+            return View();
+        }
+    }
+}
