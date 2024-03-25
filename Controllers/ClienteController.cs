21f24bdcb612ed18ad6dc569c37a4b31c8e90f27
rochagabs <gabirochasf@gmail.com>
2024-03-25 00:50:37 -0300

    tabela dos clientes

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using JitCars.Data;
+using JitCars.Models;
+
+namespace JitCars.Controllers
+{
+	public class ClienteController : Controller
+	{
+
+		readonly private AppDbContext _db;
+		public ClienteController(AppDbContext db) 
+		{
+			_db = db;
+		}
+		public IActionResult Index()
+		{
+			IEnumerable<Cliente> clientes = _db.Clientes;
+			return View(clientes);
+		}
+	}
+}
