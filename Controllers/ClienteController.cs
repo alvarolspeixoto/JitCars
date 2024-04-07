9e441f961f864d9ef3481fd36af8ebdd5888f00e
rochagabs <gabirochasf@gmail.com>
2024-04-07 18:47:43 -0300

    testando controller do cliente

--- Controllers/ClienteController.cs
+++ Controllers/ClienteController.cs
@@ -3,6 +3,7 @@ using JitCars.Data;
 using JitCars.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
+using JitCars.Enums;
 
 namespace JitCars.Controllers
 {
@@ -15,30 +16,78 @@ namespace JitCars.Controllers
         {
             _db = db;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            IEnumerable<Cliente> clientes = _db.Clientes;
+            var clientes = await _db.Clientes.ToListAsync();
             return View(clientes);
         }
 
+        [HttpGet]
         public IActionResult Cadastrar()
         {
+
             return View();
         }
 
 
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Cadastrar(ClienteEnderecoViewModel viewModel)
+        {
+            Cliente cliente = viewModel.Cliente;
+
+
+            if (_db.Clientes.Any(c => c.Cpf == cliente.Cpf))
+            {
+                ModelState.AddModelError("Cliente.Cpf", "Este CPF já está em uso");
+            }
+
+
+            if (_db.Clientes.Any(c => c.Email == cliente.Email))
+            {
+                ModelState.AddModelError("Cliente.Email", "Este endereco de e-mail já está em uso");
+            }
+
+
+            if (_db.Clientes.Any(c => c.Telefone == cliente.Telefone))
+            {
+                ModelState.AddModelError("Cliente.Telefone", "Este telefone já está em uso");
+            }
+
+
+            if (ModelState.IsValid)
+            {
+                Endereco endereco = viewModel.Endereco;
+                _db.Enderecos.Add(endereco);
+                await _db.SaveChangesAsync();
+
+                cliente.EnderecoId = endereco.Id;
+
+                _db.Clientes.Add(cliente);
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+
+            }
+
+            return View();
+        }
+
+
+
         [HttpGet]
-        /* public IActionResult Editar(int? id)
+        public async Task<IActionResult> Editar(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
 
-            // Busca o cliente pelo id fornecido, incluindo o endereço e o telefone associados
-            Cliente cliente = _db.Clientes
+            // Busca o cliente pelo id fornecido, incluindo o endereço associado
+            var cliente = await _db.Clientes
                                   .Include(c => c.Endereco)
-                                  .First(x => x.Id == id);
+                                  .FirstAsync(x => x.Id == id);
 
             // Se o cliente não for encontrado, retorna NotFound
             if (cliente == null)
@@ -49,186 +98,69 @@ namespace JitCars.Controllers
             Endereco endereco = _db.Enderecos.First(x => x.Id == cliente.EnderecoId);
 
 
-            // Busca os telefones associados ao cliente pelo ClienteId
-            Telefone telefone = _db.Telefones
-                                             .Where(t => t.ClienteId == cliente.Id)
-                                             .First();
 
             if (endereco == null)
             {
                 return NotFound();
             }
-
-            // Cria um objeto ClienteEnderecoTelViewModel e configura com o cliente e os telefones
-            ClienteEnderecoTelViewModel viewModel = new ClienteEnderecoTelViewModel
+            // Cria um objeto ClienteEnderecoViewModel e configura com o cliente e os telefones
+            ClienteEnderecoViewModel viewModel = new ClienteEnderecoViewModel
             {
                 Cliente = cliente,
-                Telefone = telefone,
                 Endereco = endereco
             };
             // Retorna a View com o cliente e seus dados relacionados
             return View(viewModel);
-        } */
-
-        /*
-		public IActionResult Editar(int? id)
-			{
-
-            if (id==null)
-			{
-				return NotFound	();
-			}
-
-            
-            Cliente cliente = _db.Clientes.First(x => x.Id == id);
-
-            if (cliente == null)
-            {
-                return NotFound();
-            }
-
-            Endereco endereco = _db.Enderecos.First(x => x.Id == cliente.EnderecoId);
-
-            if (endereco == null)
-            {
-                return NotFound();
-            }
-
-            cliente.EnderecoId = endereco.Id;
-
-            Telefone telefone = _db.Telefones.First(x => x.ClienteId == cliente.Id);
-
-			if (telefone == null)
-			{
-				return NotFound();
-			}
-
-            telefone.ClienteId = cliente.Id; 
-
-            ClienteEnderecoTelViewModel viewModel = new ClienteEnderecoTelViewModel
-            {
-                Cliente = cliente,
-                Telefone = telefone,
-                Endereco = endereco
-            };
-
-            return View(viewModel);
         }
-		*/
 
-
-        [HttpPost]
-        public IActionResult Cadastrar(ClienteEnderecoViewModel viewModel)
-        {
-            if (ModelState.IsValid)
-            {
-                Cliente cliente = viewModel.Cliente;
-                Endereco endereco = viewModel.Endereco;
-                // Telefone telefone = viewModel.Telefone;
-
-                _db.Enderecos.Add(endereco);
-                _db.SaveChanges();
-
-                cliente.EnderecoId = endereco.Id;
-                _db.Clientes.Add(cliente);
-                _db.SaveChanges();
-
-                // telefone.ClienteId = cliente.Id;
-                // _db.Telefones.Add(telefone);
-                _db.SaveChanges();
-
-                return RedirectToAction("Index");
-            }
-
-            return View();
-        }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Editar(ClienteEnderecoViewModel viewModel)
+        public async Task<IActionResult> Editar(ClienteEnderecoViewModel viewModel)
         {
-            if (ModelState.IsValid)
+            try
             {
                 Cliente cliente = viewModel.Cliente;
-                Endereco endereco = viewModel.Endereco;
-                // Telefone telefone = viewModel.Telefone;
+                var clienteDoBanco = await _db.Clientes.Include(c => c.Endereco).FirstOrDefaultAsync(c => c.Id == cliente.Id);
 
-                // Verifica se o telefone foi modificado
-                /* Telefone telefoneNoBanco = _db.Telefones.First(t => t.Id == telefone.Id);
-                if (telefoneNoBanco != null &&
-                    (telefoneNoBanco.Numero != telefone.Numero || telefoneNoBanco.ClienteId != telefone.ClienteId))
+                if (clienteDoBanco == null)
                 {
-                    // Se o telefone foi modificado, atualiza-o
-                    _db.Telefones.Update(telefone);
-                } */
-
-                // Verifica se o ID do endereço é válido
-                if (cliente.Endereco != null && cliente.Endereco.Id > 0)
-                {
-                    // Verifica se o endereço associado ao cliente está carregado corretamente
-                    endereco = _db.Enderecos.FirstOrDefault(e => e.Id == cliente.Endereco.Id);
-                    if (endereco != null)
-                    {
-                        // Mantém o "EnderecoId" original, a menos que o endereço tenha sido alterado
-                        if (cliente.EnderecoId != endereco.Id)
-                        {
-                            cliente.EnderecoId = endereco.Id;
-                        }
-                        // Atualiza o endereço associado ao cliente
-                        _db.Enderecos.Update(endereco);
-                    }
-
-
-
-                    // Atualiza o cliente
-                    _db.Clientes.Update(cliente);
-
-                    // Atualiza o telefone associado ao cliente (se existir)
-                    // _db.Telefones.Update(telefone);
-
-
-                    // Salva as alterações no banco de dados
-                    _db.SaveChanges();
-
-                    return RedirectToAction("Index");
+                    return NotFound();
                 }
 
-                // Se o modelo não for válido, retorna a View com o modelo para exibir os erros de validação
-            }
-            return View(viewModel);
+                clienteDoBanco.PrimeiroNome = cliente.PrimeiroNome;
+                clienteDoBanco.Sobrenome = cliente.Sobrenome;
+                clienteDoBanco.Email = cliente.Email;
+                clienteDoBanco.Telefone = cliente.Telefone;
+                if (clienteDoBanco.Endereco == null)
+                {
+                    clienteDoBanco.Endereco = new Endereco(); 
+                }
+                clienteDoBanco.Endereco.Cep = cliente.Endereco.Cep;
+                clienteDoBanco.Endereco.Rua = cliente.Endereco.Rua;
+                clienteDoBanco.Endereco.Numero = cliente.Endereco.Numero;
+                clienteDoBanco.Endereco.Bairro = cliente.Endereco.Bairro;
+                clienteDoBanco.Endereco.Cidade = cliente.Endereco.Cidade;
+                clienteDoBanco.Endereco.Estado = cliente.Endereco.Estado;
 
-            /*
-            public IActionResult Editar(ClienteEnderecoTelViewModel viewModel) 
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (DbUpdateConcurrencyException)
             {
 
-                if (ModelState.IsValid)
-                {
-                    Cliente cliente = viewModel.Cliente;
-                    Endereco endereco = viewModel.Endereco;
-                    Telefone telefone = viewModel.Telefone;
-
-
-
-                    _db.Enderecos.Update(endereco);
-                    _db.SaveChanges();
-
-                    cliente.EnderecoId = endereco.Id;
-                    _db.Clientes.Update(cliente);
-                    _db.SaveChanges();
-
-                    telefone.ClienteId = cliente.Id;
-                    _db.Telefones.Update(telefone);
-                    _db.SaveChanges();
-
-
-                    return RedirectToAction("Index");
-                }
-
-                return View();
+                ModelState.AddModelError("", "Os dados foram modificados por outro usuário. Por favor, tente novamente.");
+                return View(viewModel);
             }
-            */
+
+
 
 
         }
     }
 }
+
+
+
+
+
